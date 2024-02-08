7c204c1f3ea192868962067b6b8929e47f78211d
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2024-02-08 20:31:22 +0530

    js refresher

---
+++ play.js
@@ -0,0 +1,10 @@
+var name='prabhat';
+var age=20;
+var hasHobbies=true;
+
+
+function summarizeUser(Username,userage,userhashobbies){
+    return `Name is ${Username} age is ${userage} and the user has hobbies:${userhashobbies}`
+}
+
+console.log(summarizeUser(name,age,hasHobbies))
